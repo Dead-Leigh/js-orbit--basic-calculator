f416aa117f943625b4b7f71c0e689d047ad55fca
Leigh Carr <leighcarr@Leighs-MacBook-Pro.local>
2019-07-20 18:30:34 -0400

    math man

---
+++ script.js
@@ -0,0 +1,26 @@
+function calcMultiply(){
+  var num1 = document.querySelector('#num1').value;
+  var num2 = document.querySelector('#num2').value;
+
+  var multAnswer = (num1 * num2);
+  var htmlElement = document.querySelector('#result');
+  htmlElement.innerHTML = num1 * num2;
+  // alert()
+  //get second number value and perform fun.
+}
+
+//function calcDivid
+
+function calcDivid(){
+  var num1 = document.querySelector('#num1').value;
+  var num2 = document.querySelector('#num2').value;
+
+  var dividAnswer = (num1 / num2);
+  var htmlElement = document.querySelector('#result');
+  // alert(num1 / num2);
+  htmlElement.innerHTML = num1 / num2;
+}
+
+// look up operaters for math
+
+// 
